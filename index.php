aae62114d1890604969a7fe35906aa398960ce20
The-B3au-Champ <charlesb3au@hotmail.com>
2024-09-17 17:00:13 -0400

    Creation php et styles

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+    <form action="">
+        <h1>Gestion des étudiants</h1>
+        <label for="prenom">Prénom:</label>
+        <input type="text" name="prenom">
+        <label for="nom">Nom de famille:</label>
+        <input type="text" name="nom">
+        <label for="date">Date de naissance:</label>
+        <input type="date" name="date">
+        <label for="email">E-mail</label>
+        <input type="text" name="email">
+
+        <button>Ajouter un étudiant</button>
+
+        <h1>Liste des étudiants</h1>
+        <table>
+        <thead>
+            <tr>
+                <th>Prénom</th>
+                <th>Nom de famille</th>
+                <th>Date de naissance</th>
+                <th>E-mail</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tbody>
+        </table>
+    </form>
+</body>
+</html>
